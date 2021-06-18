8b0f28d2b07d693142601c052296365c15442fd2
Jamshid <mamatovjamshid506@gmail.com>
2021-06-18 19:39:08 +0500

    add tasks route to project

---
+++ todoApi/urls.py
@@ -0,0 +1,10 @@
+from django import urls
+from django.contrib import admin
+from django.urls import path
+from django.urls.conf import include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('tasks/',include('tasks.urls')),
+  
+]
